e05a90ec3848929577b715b51ce38109f2458c08
Peter Vorwieger <peter@Peters-MacBook-Pro.local>
2014-12-08 19:24:58 +0100

    DDHotKey

--- HotKey/AppDelegate.m
+++ HotKey/AppDelegate.m
@@ -7,6 +7,9 @@
 //
 
 #import "AppDelegate.h"
+#import "DDHotKeyCenter.h"
+#import <Carbon/Carbon.h>
+//  #import <AppKit/AppKit.h>
 
 @interface AppDelegate ()
 
@@ -15,7 +18,24 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    
+    NSString *source = @"tell application \"Finder\" to set myname to POSIX path of (target of window 1 as alias)";
+    NSAppleScript *script = [[NSAppleScript alloc] initWithSource:source];
+    
+    DDHotKeyTask task = ^(NSEvent *hkEvent) {
+        NSAppleEventDescriptor *scriptResult = [script executeAndReturnError:nil];
+        NSString *path = [scriptResult stringValue];
+        NSTask *task = [[NSTask alloc] init];
+        task.launchPath = @"/usr/bin/open";
+        task.arguments  = @[@"-a",@"Terminal",path];
+        [task launch];
+    };
+    
+    DDHotKeyCenter *c = [DDHotKeyCenter sharedHotKeyCenter];
+    if (![c registerHotKeyWithKeyCode:kVK_Return modifierFlags:NSCommandKeyMask | NSShiftKeyMask task:task]) {
+        //[self addOutput:@"Unable to register hotkey for example 1"];
+    }
+
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
