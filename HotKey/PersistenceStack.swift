fac6f4b5e3fb2277cdb3855e560cb199d5730a75
Peter Vorwieger <peter.vorwieger@me.com>
2015-01-27 20:11:38 +0100

    first shot of PersistenceStack

---
+++ HotKey/PersistenceStack.swift
@@ -0,0 +1,82 @@
+//
+//  PersistenceStack.swift
+//  HotKey
+//
+//  Created by Peter Vorwieger on 26.01.15.
+//  Copyright (c) 2015 Peter Vorwieger. All rights reserved.
+//
+
+import Cocoa
+
+class PersistenceStack: NSObject {
+    
+    lazy var applicationDocumentsDirectory: NSURL = {
+        // The directory the application uses to store the Core Data store file. This code uses a directory named "de.peter-vorwieger.onk" in the user's Application Support directory.
+        let urls = NSFileManager.defaultManager().URLsForDirectory(.ApplicationSupportDirectory, inDomains: .UserDomainMask)
+        let appSupportURL = urls[urls.count - 1] as NSURL
+        return appSupportURL.URLByAppendingPathComponent("de.peter-vorwieger.onk")
+    }()
+    
+    lazy var managedObjectModel: NSManagedObjectModel = {
+        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
+        let modelURL = NSBundle.mainBundle().URLForResource("onk", withExtension: "momd")!
+        return NSManagedObjectModel(contentsOfURL: modelURL)!
+    }()
+    
+    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
+        // The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it. (The directory for the store is created, if necessary.) This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
+        let fileManager = NSFileManager.defaultManager()
+        var shouldFail = false
+        var error: NSError? = nil
+        var failureReason = "There was an error creating or loading the application's saved data."
+        
+        // Make sure the application files directory is there
+        let propertiesOpt = self.applicationDocumentsDirectory.resourceValuesForKeys([NSURLIsDirectoryKey], error: &error)
+        if let properties = propertiesOpt {
+            if !properties[NSURLIsDirectoryKey]!.boolValue {
+                failureReason = "Expected a folder to store application data, found a file \(self.applicationDocumentsDirectory.path)."
+                shouldFail = true
+            }
+        } else if error!.code == NSFileReadNoSuchFileError {
+            error = nil
+            fileManager.createDirectoryAtPath(self.applicationDocumentsDirectory.path!, withIntermediateDirectories: true, attributes: nil, error: &error)
+        }
+        
+        // Create the coordinator and store
+        var coordinator: NSPersistentStoreCoordinator?
+        if !shouldFail && (error == nil) {
+            coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+            let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("onk.storedata")
+            if coordinator!.addPersistentStoreWithType(NSXMLStoreType, configuration: nil, URL: url, options: nil, error: &error) == nil {
+                coordinator = nil
+            }
+        }
+        
+        if shouldFail || (error != nil) {
+            // Report any error we got.
+            let dict = NSMutableDictionary()
+            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
+            dict[NSLocalizedFailureReasonErrorKey] = failureReason
+            if error != nil {
+                dict[NSUnderlyingErrorKey] = error
+            }
+            error = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
+            NSApplication.sharedApplication().presentError(error!)
+            return nil
+        } else {
+            return coordinator
+        }
+    }()
+    
+    lazy var managedObjectContext: NSManagedObjectContext? = {
+        // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
+        let coordinator = self.persistentStoreCoordinator
+        if coordinator == nil {
+            return nil
+        }
+        var managedObjectContext = NSManagedObjectContext()
+        managedObjectContext.persistentStoreCoordinator = coordinator
+        return managedObjectContext
+    }()
+
+}
