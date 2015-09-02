046daab52c412ad17140888943dda5eb7a372733
Peter Vorwieger <peter.vorwieger@me.com>
2015-09-03 01:13:30 +0200

    Label centered, background light translucent

--- HotKey/LabelView.swift
+++ HotKey/LabelView.swift
@@ -18,16 +18,27 @@ class LabelView: NSView {
     
     override func drawRect(dirtyRect: NSRect) {
         super.drawRect(dirtyRect)
-        NSColor.windowBackgroundColor().set()
+//        NSColor.windowBackgroundColor().set()
+//        NSColor.clearColor().set()
+        NSColor(white: 0.9, alpha: 0.9).set()
         NSRectFill(dirtyRect)
         text.drawInRect(frame, withAttributes: attributes)
     }
+  
+    
     
     var attributes:[String:AnyObject] {
         get {
             let fontSize = calculateFontSize(frame.size)
             let font = NSFont.systemFontOfSize(fontSize)
-            return [NSFontAttributeName: font]
+            let color = NSColor.textColor()
+            let style = NSMutableParagraphStyle()
+            style.alignment = NSTextAlignment.Center
+            return [
+                NSFontAttributeName: font,
+                NSForegroundColorAttributeName: color,
+                NSParagraphStyleAttributeName: style
+            ]
         }
     }
     
@@ -37,7 +48,7 @@ class LabelView: NSView {
         let sampleSize = text.sizeWithAttributes(attributes)
         let width = size.width - 20
         let height = size.height
-        let scale = min(width / (sampleSize.width), height / sampleSize.height)
+        let scale = min(width / sampleSize.width, height / sampleSize.height)
         return scale * sampleFont.pointSize
     }
     
@@ -46,7 +57,7 @@ class LabelView: NSView {
         let font = NSFont.systemFontOfSize(fontSize)
         let size = text.sizeWithAttributes([NSFontAttributeName: font])
         let width = min(original.width, size.width)
-        let height = min(original.height,size.height)
+        let height = min(original.height, size.height)
         return NSSize(width: width, height:height)
     }
     
